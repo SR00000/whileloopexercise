b33a34829839a4cd72b421fa8aa24d811a642006
SR00000 <40726606+SR00000@users.noreply.github.com>
2022-04-13 20:53:14 -0400

    first commit

---
+++ myscript.js
@@ -0,0 +1,17 @@
+var x = 0;
+
+while (x <5) {
+  console.log("x is currently: "+x);
+
+  if (x===3) {
+    console.log("X IS EQUAL TO 3");
+  }
+
+  console.log("x is still less than 5, adding 1 to x");
+
+  x = x+1;
+
+  if (x===5) {
+    console.log("x is now equal to 5!");
+  }
+}
